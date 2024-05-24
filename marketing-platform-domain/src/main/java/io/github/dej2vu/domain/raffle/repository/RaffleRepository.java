65f3213b65f53504298a180198f84aa5b0456aef
dej2vu <dej2vu.tech@gmail.com>
2024-05-24 09:08:45 +0800

    feat: add RedisService

---
+++ marketing-platform-domain/src/main/java/io/github/dej2vu/domain/raffle/repository/RaffleRepository.java
@@ -0,0 +1,4 @@
+package io.github.dej2vu.domain.raffle.repository;
+
+public interface RaffleRepository {
+}
---
+++ marketing-platform-infrastructure/src/main/java/io/github/dej2vu/infrastructure/persistent/raffle/repository/RaffleRepositoryImpl.java
@@ -0,0 +1,8 @@
+package io.github.dej2vu.infrastructure.persistent.raffle.repository;
+
+import io.github.dej2vu.domain.raffle.repository.RaffleRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RaffleRepositoryImpl implements RaffleRepository {
+}
