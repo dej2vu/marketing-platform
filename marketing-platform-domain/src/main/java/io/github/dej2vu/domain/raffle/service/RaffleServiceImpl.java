a5858b611edec97a5972c49237f0340922273185
dej2vu <dej2vu.tech@gmail.com>
2024-05-24 12:42:54 +0800

    feat: Assemble Strategy
    (cherry picked from commit 1a1b420da84e80aa973d7a0e6cbcbb186ec844f0)

--- marketing-platform-domain/src/main/java/io/github/dej2vu/domain/raffle/service/RaffleServiceImpl.java
+++ marketing-platform-domain/src/main/java/io/github/dej2vu/domain/raffle/service/RaffleServiceImpl.java
@@ -1,8 +1,19 @@
 package io.github.dej2vu.domain.raffle.service;
 
+import io.github.dej2vu.domain.raffle.model.RaffleStrategyPrize;
 import io.github.dej2vu.domain.raffle.repository.RaffleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +23,56 @@ public class RaffleServiceImpl implements RaffleService {
 
     @Override
     public boolean assembleStrategy(String strategyCode) {
-        return false;
+        // 1. 获取策略配置
+        List<RaffleStrategyPrize> strategyPrizes = raffleRepository.findStrategyPrizeByStrategyCode(strategyCode);
+
+        // 2. 构造奖品查找表
+        Map<Integer, String> prizeSearchTable = buildPrizeSearchTable(strategyPrizes);
+
+        // 3. 存储至 Redis
+        raffleRepository.cachePrizeSearchTable(strategyCode, prizeSearchTable);
+        return true;
+    }
+
+    private Map<Integer, String> buildPrizeSearchTable(List<RaffleStrategyPrize> strategyPrizes){
+        // 1. 获取 minRate
+        BigDecimal minRate = strategyPrizes.stream()
+                .map(RaffleStrategyPrize::getRate)
+                .min(BigDecimal::compareTo)
+                .orElseThrow();
+
+        // 2. 获取 totalRate
+        BigDecimal totalRate = strategyPrizes.stream().map(RaffleStrategyPrize::getRate)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        // 3. 用 totalRate % minRate 获得概率范围，百分位、千分位、万分位
+        BigDecimal rateRange = totalRate.divide(minRate, 0, RoundingMode.CEILING);
+
+        // 4. 根据奖品概率生成奖品查询表
+        List<String> prizeSearchTable = new ArrayList<>(rateRange.intValue());
+        for (RaffleStrategyPrize strategyPrize : strategyPrizes) {
+            String prizeCode = strategyPrize.getPrizeCode();
+            BigDecimal rate = strategyPrize.getRate();
+            // 计算出每个概率值需要存放到查找表的数量，循环填充
+            for (int i = 0; i < rateRange.multiply(rate).setScale(0, RoundingMode.CEILING).intValue(); i++) {
+                prizeSearchTable.add(prizeCode);
+            }
+        }
+
+        // 5. 对奖品进行乱序处理
+        Collections.shuffle(prizeSearchTable);
+
+        // 6. 生成奖品查找表(HashMap), key为乱序后的index，value为奖品编码，后续可使用随机值作为key来查找对用的奖品编码
+        return IntStream.range(0, prizeSearchTable.size())
+                .boxed()
+                .collect(Collectors.toMap(i -> i, prizeSearchTable::get));
     }
 
     @Override
     public String getRandomResult(String strategyCode) {
-        return "";
+
+        int rateRange = raffleRepository.getRateRange(strategyCode);
+
+        return raffleRepository.getPrizeCodeFromPrizeSearchTable(strategyCode, new SecureRandom().nextInt(rateRange));
     }
 }
--- marketing-platform-infrastructure/src/main/java/io/github/dej2vu/infrastructure/persistent/raffle/repository/RaffleRepositoryImpl.java
+++ marketing-platform-infrastructure/src/main/java/io/github/dej2vu/infrastructure/persistent/raffle/repository/RaffleRepositoryImpl.java
@@ -1,8 +1,57 @@
 package io.github.dej2vu.infrastructure.persistent.raffle.repository;
 
+import io.github.dej2vu.constant.Constants;
+import io.github.dej2vu.domain.raffle.model.RaffleStrategyPrize;
 import io.github.dej2vu.domain.raffle.repository.RaffleRepository;
+import io.github.dej2vu.infrastructure.persistent.raffle.convertor.RaffleStrategyPrizeConvertor;
+import io.github.dej2vu.infrastructure.persistent.raffle.mapper.RaffleStrategyPrizeMapper;
+import io.github.dej2vu.infrastructure.redis.RedisService;
+import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
+import java.util.Map;
 
 @Repository
 public class RaffleRepositoryImpl implements RaffleRepository {
+
+    @Resource
+    private RaffleStrategyPrizeMapper raffleStrategyPrizeMapper;
+    @Resource
+    private RedisService redisService;
+
+    @Override
+    public List<RaffleStrategyPrize> findStrategyPrizeByStrategyCode(String strategyCode) {
+
+        // 优先从缓存获取
+        String cacheKey = Constants.RedisKey.RAFFLE_STRATEGY_PRIZE_LIST_KEY + strategyCode;
+        List<RaffleStrategyPrize> cachedStrategyPrizes = redisService.getValue(cacheKey);
+
+        if (!CollectionUtils.isEmpty(cachedStrategyPrizes)) {return cachedStrategyPrizes;}
+
+        List<RaffleStrategyPrize> list = RaffleStrategyPrizeConvertor.convert(raffleStrategyPrizeMapper.findByStrategyCode(strategyCode));
+        redisService.setValue(cacheKey, list);
+        return list;
+    }
+
+    @Override
+    public void cachePrizeSearchTable(String strategyCode, Map<Integer, String> prizeSearchTable) {
+        // 1. 存储抽奖策略范围值，如10000，用于生成1000以内的随机数
+        redisService.setValue(Constants.RedisKey.RAFFLE_STRATEGY_RATE_RANGE_KEY + strategyCode, prizeSearchTable.size());
+        // 2. 存储概率查找表
+        Map<Integer, String> cachePrizeSearchTable = redisService.getMap(Constants.RedisKey.RAFFLE_STRATEGY_PRIZE_SEARCH_TABLE_KEY + strategyCode);
+        cachePrizeSearchTable.putAll(prizeSearchTable);
+    }
+
+    @Override
+    public int getRateRange(String strategyCode) {
+        return redisService.getValue(Constants.RedisKey.RAFFLE_STRATEGY_RATE_RANGE_KEY + strategyCode);
+    }
+
+    @Override
+    public String getPrizeCodeFromPrizeSearchTable(String strategyCode, Integer rateKey) {
+        return redisService.getFromMap(Constants.RedisKey.RAFFLE_STRATEGY_PRIZE_SEARCH_TABLE_KEY + strategyCode, rateKey);
+    }
 }
